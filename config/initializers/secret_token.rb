f2a3023c29fd15e9913a5ac6fc1db0f6540b8a96
Vipul <vipul@vipul.(none)>
2015-09-14 18:02:40 +0530

    football moved

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Football::Application.config.secret_token = 'b856ba43efca41014850a1073343dda046a9edf62abfa1ec137e14a89c0ec9965099c7d99c007c61730cf3a6d62fb1b4313e60a3945138b51b246278515a6514'
